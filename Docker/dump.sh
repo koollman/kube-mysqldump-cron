f97d798b9794a16e6e54d671c4eb2db9d1fd4096
Thomas Samson <tsamson@advanced-schema.com>
2018-04-30 13:29:15 +0200

    handle multiple db hosts

--- Docker/dump.sh
+++ Docker/dump.sh
@@ -4,6 +4,7 @@ DB_USER=${DB_USER:-${MYSQL_ENV_DB_USER}}
 DB_PASS=${DB_PASS:-${MYSQL_ENV_DB_PASS}}
 DB_NAME=${DB_NAME:-${MYSQL_ENV_DB_NAME}}
 DB_HOST=${DB_HOST:-${MYSQL_ENV_DB_HOST}}
+DB_HOSTS=${DB_HOSTS:-${MYSQL_ENV_DB_HOSTS}}
 ALL_DATABASES=${ALL_DATABASES}
 IGNORE_DATABASE=${IGNORE_DATABASE}
 
@@ -16,23 +17,29 @@ if [[ ${DB_PASS} == "" ]]; then
 	echo "Missing DB_PASS env variable"
 	exit 1
 fi
-if [[ ${DB_HOST} == "" ]]; then
-	echo "Missing DB_HOST env variable"
+if [[ ${DB_HOSTS} == "" ]]; then
+    if [[ ${DB_HOST} == "" ]]; then
+	echo "Missing DB_HOSTS or DB_HOST env variable"
 	exit 1
+    else
+	$DB_HOSTS=$DB_HOST
+    fi
 fi
 
-if [[ ${ALL_DATABASES} == "" ]]; then
+for DB_HOST in $DB_HOSTS; do
+    if [[ ${ALL_DATABASES} == "" ]]; then
 	if [[ ${DB_NAME} == "" ]]; then
 		echo "Missing DB_NAME env variable"
 		exit 1
 	fi
 	mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > /mysqldump/"${DB_NAME}".sql
-else
+    else
 	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
-for db in $databases; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-        echo "Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/$db.sql
+	for db in $databases; do
+	    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
+		echo "Dumping database: $db"
+		mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/${DB_HOST}__$db.sql
+	    fi
+	done
     fi
 done
-fi
